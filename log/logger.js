4882f3a11baffb0426f2e98fae152fbbcc3453f8
Ausaf Elahi <143334289+ausafelahi@users.noreply.github.com>
2026-04-30 23:12:12 +0500

    Simplify logger setup for production and development
    Refactor logger configuration for production environment.

--- log/logger.js
+++ log/logger.js
@@ -1,26 +1,14 @@
 import "dotenv/config";
 import pino from "pino";
 
-const isDev = process.env.NODE_ENV !== "production";
-
-const logger = pino({
-  level: isDev ? "info" : "debug",
-  transport: isDev
-    ? {
-        target: "pino-pretty",
-        options: {
-          colorize: true,
-          translateTime: "SYS:standard",
-          ignore: "pid,hostname",
+const logger = pino(
+  process.env.NODE_ENV === "production"
+    ? {}
+    : {
+        transport: {
+          target: "pino-pretty",
         },
-      }
-    : undefined,
-  formatters: {
-    level: (label) => {
-      return { level: label };
-    },
-  },
-  timestamp: () => `,"time":"${new Date().toISOString()}"`,
-});
+      },
+);
 
 export default logger;
